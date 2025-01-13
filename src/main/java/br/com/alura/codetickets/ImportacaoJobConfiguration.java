279e2db68281353742c4b392a19a1397682c0d3c
Kaique <kaique.andruid@gmail.com>
2025-01-13 19:27:24 -0300

    Job method , jobBuilder

---
+++ src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.alura.codetickets;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ImportacaoJobConfiguration {
+
+
+    @Bean
+    public Job job(Step passoInicial, JobRepository jobRepository) {
+
+        return new JobBuilder("geracao-tickets",jobRepository)
+                .start(passoInicial)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+}
